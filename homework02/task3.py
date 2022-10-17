ed62459f7b166a5cd4f59a389e216f9ac12b2203
Anastasia <anastasia.levina@geekbrains.ru>
2022-10-18 00:04:56 +0300

    all attempts were failed

--- homework02/task3.py
+++ homework02/task3.py
@@ -1,11 +1,14 @@
 # Задача 3. Напишите программу, в которой пользователь будет задавать две строки, 
 # а программа - определять количество вхождений одной строки в другой
 
-list_1 = ('введите строку 1: ')
-list_2 = ('введите строку 2: ')
+list_1 = input('введите строку 1: ')
+list_2 = input('введите строку 2: ')
 count = 0
 if len(list_1) > len(list_2):
-    while list_2 in list_1:
-        count +=1
-        list_1.remove(list_2)
+    for list_2 in list_1:
+        a = len(list_2)
+        for i in a:
+            if list_1[i] == list_2[i]:
+                i +=1
+            count +=1
 print(count)
